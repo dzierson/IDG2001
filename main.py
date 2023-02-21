0f842cae272c63ca9d1c3789a51a36256e4c7eca
Kris <ann.dzierson@gmail.com>
2023-02-21 14:55:13 +0100

    Initializing the Railway Project.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    for _ in range(50):
+        return "<p>Hello, Railway! :D</p>"
+    time.sleep(5)
+
+if __name__ == '__main__':
+    app.run()
